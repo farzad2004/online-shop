c563465cbe7878c2ee83b4a23635926a2ecf3f7d
farzad2004 <farzad.alamdar2008@gmail.com>
2020-05-06 22:58:23 +1000

    lecture 65 pqrt 1

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./menu-item.styles.scss";
+const MenuItem = ({ title }) => (
+  <div className="menu-item">
+    <div className="cotent">
+      <h1 className="title">{title}</h1>
+      <span className="subtitLe">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
