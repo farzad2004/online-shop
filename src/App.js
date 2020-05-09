b9301b45f78883c0ec1f9b24c55450a8e890e566
farzad2004 <farzad.alamdar2008@gmail.com>
2020-05-10 00:16:50 +1000

    create sign-in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
+import SignInAndSignUpPgae from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
+
 const HasJackets = () => (
   <div>
     <h1>JACKETS PAGE </h1>
@@ -17,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/sign-in" component={SignInAndSignUpPgae} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.styles.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
+const SignInAndSignUpPgae = () => <SignIn />;
+export default SignInAndSignUpPgae;
