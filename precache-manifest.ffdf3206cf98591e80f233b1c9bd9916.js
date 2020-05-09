33b9c7693621c3150cc405059c6b12c20c667b03
farzad2004 <farzad.alamdar2008@gmail.com>
2020-05-09 22:16:05 +1000

    Updates

---
+++ precache-manifest.ffdf3206cf98591e80f233b1c9bd9916.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cda13726dfaaf246a9b25354aa5e430",
+    "url": "//online-shop/index.html"
+  },
+  {
+    "revision": "d5d3a581997deba1d0ae",
+    "url": "//online-shop/static/css/main.ccf2e4e1.chunk.css"
+  },
+  {
+    "revision": "7e94121f431de7bc87ce",
+    "url": "//online-shop/static/js/2.0d2b3052.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//online-shop/static/js/2.0d2b3052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d3a581997deba1d0ae",
+    "url": "//online-shop/static/js/main.ec8735fc.chunk.js"
+  },
+  {
+    "revision": "a51cd8c872ceac95d041",
+    "url": "//online-shop/static/js/runtime-main.ed2dde41.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "//online-shop/static/media/crown.ed28accb.svg"
+  }
+]);
